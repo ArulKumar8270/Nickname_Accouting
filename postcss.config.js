dbaa3c94685cf069e65b328979cbbf09159af120
Afrinaapi1329 <afrinmuchu@gmail.com>
2026-05-13 16:33:58 +0530

    Resolved merge conflicts

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['@reduxjs/toolkit'],
-    exclude: [],
-  },
-  resolve: {
-    alias: {
-      // Prevents dual package issue
-    }
-  }
-})
-=======
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
->>>>>>> 14aee8150c5bf87a278071cb51b69b8b2bef1fad
